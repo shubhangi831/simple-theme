9064bb6fb468bc5246aac58155a5247cd0821e7b
shubhangi831 <126654631+shubhangi831@users.noreply.github.com>
2023-07-01 15:37:22 +0530

    first commit

---
+++ archive-event.php
@@ -0,0 +1,35 @@
+<?php 
+get_header(); 
+pageBanner(array(
+  'title' => 'All Events',
+  'subtitle' => 'See what is going on in our world.'
+));
+?>
+
+    <!-- <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">All Events
+       
+        </h1>
+        <div class="page-banner__intro">
+          <p>See what is going on in our world.</p>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="container container--narrow page-section">
+      <?php
+        while(have_posts()){
+          the_post();
+          get_template_part('template-parts/content-event');
+        }
+       echo paginate_links();
+      ?>
+
+     <hr class="section-break">
+
+      <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>"> check out our past events archive.</a></p>
+    </div>
+<?php
+get_footer(); ?>
---
+++ page-past-events.php
@@ -0,0 +1,54 @@
+<?php 
+get_header(); 
+pageBanner(array(
+  'title' => 'Past Events',
+  'subtitle' => 'A recap of our past events.'
+));
+?>
+
+    <!-- <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">Past Events
+       
+        </h1>
+        <div class="page-banner__intro">
+          <p>A recap of our past events.</p>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="container container--narrow page-section">
+      <?php
+
+        $today = date('Ymd');
+        $pastEvents = new WP_Query(array(
+        'paged' => get_query_var('paged', 1),
+        // 'posts_per_page' => 1,
+        'post_type' => 'event',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+            array(
+            'key' => 'event_date',
+            'compare' => '<',
+            'value' => $today,
+            'type' => 'numeric'
+            )
+        )
+        
+        ));
+
+
+        while($pastEvents->have_posts()){
+            $pastEvents->the_post(); 
+            get_template_part('template-parts/content-event');
+          }
+       echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+       ));
+      ?>
+    </div>
+<?php
+get_footer(); ?>
